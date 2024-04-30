6326c2b3b5399eacec393f8a0dc94c6e87e54483
JoaoHenriqueProg <joaohenriquejh.mds@gmail.com>
2024-04-29 21:41:43 -0300

    Classes básicas

---
+++ src/main/java/Model/Amigo.java
@@ -0,0 +1,8 @@
+package Model;
+
+public class Amigo {
+    private String nome;
+    private String endereco;
+    private String cpf;
+    private int id;
+}
---
+++ src/main/java/Model/Ferramenta.java
@@ -0,0 +1,7 @@
+package Model;
+
+public class Ferramenta {
+    private String nome;
+    private int id;
+    private double preco_de_aquisicao;
+}
