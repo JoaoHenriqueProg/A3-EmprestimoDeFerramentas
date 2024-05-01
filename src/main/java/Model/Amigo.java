a075d459e8d322423c38dc491a3c56ebd0bab979
JoaoHenriqueProg <joaohenriquejh.mds@gmail.com>
2024-05-01 16:52:14 -0300

    feat: Adicionado métodos as classes básicas

--- src/main/java/Model/Amigo.java
+++ src/main/java/Model/Amigo.java
@@ -5,4 +5,47 @@ public class Amigo {
     private String endereco;
     private String cpf;
     private int id;
+
+    public Amigo() {
+        this("", "", "", 0);
+    }
+
+    public Amigo(String nome, String endereco, String cpf, int id) {
+        this.nome = nome;
+        this.endereco = endereco;
+        this.cpf = cpf;
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
--- src/main/java/Model/Ferramenta.java
+++ src/main/java/Model/Ferramenta.java
@@ -4,4 +4,38 @@ public class Ferramenta {
     private String nome;
     private int id;
     private double preco_de_aquisicao;
+
+    public Ferramenta() {
+        this("", 0, 0);
+    }
+
+    public Ferramenta(String nome, int id, double preco_de_aquisicao) {
+        this.nome = nome;
+        this.id = id;
+        this.preco_de_aquisicao = preco_de_aquisicao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getPreco_de_aquisicao() {
+        return preco_de_aquisicao;
+    }
+
+    public void setPreco_de_aquisicao(double preco_de_aquisicao) {
+        this.preco_de_aquisicao = preco_de_aquisicao;
+    }
 }
