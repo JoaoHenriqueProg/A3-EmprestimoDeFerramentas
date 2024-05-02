a4eee571920311be94ada70b9c1883f706c6e698
leand <leand@Leandro>
2024-05-01 21:20:17 -0300

    feat: JFMenu

--- src/main/java/Principal/Main.java
+++ src/main/java/Principal/Main.java
@@ -1,7 +1,9 @@
 package Principal;
+import Telas.JFMenu;
 
 public class Main {
     public static void main(String[] args) {
-
+            JFMenu objetotela = new JFMenu(); 
+            objetotela.setVisible(true);
     }
 }
