9a953bda56757ac4ce339e819b4027744ba41a3f
y-leandro <tomatinhopicado@gmail.com>
2024-05-14 19:55:41 -0300

    test: commit test

--- src/main/java/Model/Ferramenta.java
+++ src/main/java/Model/Ferramenta.java
@@ -9,7 +9,7 @@ public class Ferramenta {
         this("", 0, 0);
     }
 
-    // commit teste
+    // commit teste 2
     public Ferramenta(String nome, int id, double preco_de_aquisicao) {
         this.nome = nome;
         this.id = id;
