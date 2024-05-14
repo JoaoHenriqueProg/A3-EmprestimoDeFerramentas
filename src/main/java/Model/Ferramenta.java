eaa5206d6a396cfb377abd9c3be7633b77bdbf6c
leand <leand@Leandro>
2024-05-14 19:51:55 -0300

    test: commit test

--- src/main/java/Model/Ferramenta.java
+++ src/main/java/Model/Ferramenta.java
@@ -9,6 +9,7 @@ public class Ferramenta {
         this("", 0, 0);
     }
 
+    // commit teste
     public Ferramenta(String nome, int id, double preco_de_aquisicao) {
         this.nome = nome;
         this.id = id;
