bb21e8b217afeee04ea88bf10271370d93f7308a
leand <leand@Leandro>
2024-05-13 21:30:27 -0300

    feat:

--- src/main/java/Telas/JFMenu.java
+++ src/main/java/Telas/JFMenu.java
@@ -181,6 +181,7 @@ public class JFMenu extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void jMenuItemAddClienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItemAddClienteActionPerformed
