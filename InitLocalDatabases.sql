2170bf2aa6bce1257ac1389bee99c9683112fb39
JoaoHenriqueProg <joaohenriquejh.mds@gmail.com>
2024-05-16 19:11:25 -0300

    docs: documentação em InitLocalDatabases.sql

--- InitLocalDatabases.sql
+++ InitLocalDatabases.sql
@@ -1,9 +1,14 @@
+# Cria as bases de dados locais, limpando as que já existem
+
+# Cria a tabela de ferramentas
 CREATE DATABASE IF NOT EXISTS TBFerramentas;
 USE TBFerramentas;
 DROP TABLE IF EXISTS TBFerramentas;
 CREATE TABLE TBFerramentas (
-id INTEGER NOT NULL,
-nome VARCHAR(100),
-preco DOUBLE,
-PRIMARY KEY(id)
+	id INTEGER NOT NULL,
+	nome VARCHAR(100),
+	preco DOUBLE,
+	PRIMARY KEY(id)
 );
+
+# TODO: Tabela de amigos
