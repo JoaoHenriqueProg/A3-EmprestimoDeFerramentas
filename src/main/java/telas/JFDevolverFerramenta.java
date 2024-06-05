4a65d307ff500e6e0ec364c709abc270b647be34
venuslima <168045936+venuslima@users.noreply.github.com>
2024-06-05 18:28:31 -0300

    feat(JFDevolverFerramenta): elementos da tela

--- src/main/java/telas/JFDevolverFerramenta.java
+++ src/main/java/telas/JFDevolverFerramenta.java
@@ -4,6 +4,8 @@
  */
 package telas;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author busin
@@ -27,21 +29,95 @@ public class JFDevolverFerramenta extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
+        JLAmigo = new javax.swing.JLabel();
+        JCBAmigo = new javax.swing.JComboBox<>();
+        JLAluguel = new javax.swing.JLabel();
+        JCBAluguel = new javax.swing.JComboBox<>();
+        JLQuantidadeDevolver = new javax.swing.JLabel();
+        JSpinnerQuantidadeDevolver = new javax.swing.JSpinner();
+        JBDevolverFerramenta = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setPreferredSize(new java.awt.Dimension(427, 299));
 
         jPanel1.setBackground(new java.awt.Color(54, 70, 125));
 
+        JLAmigo.setFont(new java.awt.Font("Comic Sans MS", 1, 18)); // NOI18N
+        JLAmigo.setForeground(new java.awt.Color(242, 242, 242));
+        JLAmigo.setText("Amigo");
+
+        JCBAmigo.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        JCBAmigo.setPreferredSize(new java.awt.Dimension(72, 27));
+
+        JLAluguel.setFont(new java.awt.Font("Comic Sans MS", 1, 18)); // NOI18N
+        JLAluguel.setForeground(new java.awt.Color(242, 242, 242));
+        JLAluguel.setText("Aluguel");
+
+        JCBAluguel.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        JCBAluguel.setPreferredSize(new java.awt.Dimension(72, 27));
+
+        JLQuantidadeDevolver.setFont(new java.awt.Font("Comic Sans MS", 1, 18)); // NOI18N
+        JLQuantidadeDevolver.setForeground(new java.awt.Color(242, 242, 242));
+        JLQuantidadeDevolver.setText("Quantidade a devolver");
+
+        JSpinnerQuantidadeDevolver.setPreferredSize(new java.awt.Dimension(64, 27));
+
+        JBDevolverFerramenta.setBackground(new java.awt.Color(73, 159, 104));
+        JBDevolverFerramenta.setFont(new java.awt.Font("Comic Sans MS", 1, 14)); // NOI18N
+        JBDevolverFerramenta.setForeground(new java.awt.Color(240, 240, 240));
+        JBDevolverFerramenta.setText("Devolver");
+        JBDevolverFerramenta.setPreferredSize(new java.awt.Dimension(120, 28));
+        JBDevolverFerramenta.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                JBDevolverFerramentaActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(JLQuantidadeDevolver)
+                .addGap(99, 99, 99))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(73, 73, 73)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(JLAmigo)
+                            .addComponent(JLAluguel))
+                        .addGap(18, 18, 18)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(JCBAluguel, javax.swing.GroupLayout.PREFERRED_SIZE, 162, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(JCBAmigo, javax.swing.GroupLayout.PREFERRED_SIZE, 162, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(133, 133, 133)
+                        .addComponent(JBDevolverFerramenta, javax.swing.GroupLayout.PREFERRED_SIZE, 130, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(154, 154, 154)
+                        .addComponent(JSpinnerQuantidadeDevolver, javax.swing.GroupLayout.PREFERRED_SIZE, 92, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(86, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(58, 58, 58)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(JLAmigo, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(JCBAmigo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(JLAluguel, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(JCBAluguel, javax.swing.GroupLayout.DEFAULT_SIZE, 28, Short.MAX_VALUE))
+                .addGap(18, 18, 18)
+                .addComponent(JLQuantidadeDevolver)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(JSpinnerQuantidadeDevolver, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(JBDevolverFerramenta, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(37, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -57,6 +133,18 @@ public class JFDevolverFerramenta extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void JBDevolverFerramentaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_JBDevolverFerramentaActionPerformed
+        // TODO add your handling code here:
+        if (JCBAmigo.getSelectedItem() == null) {
+            JOptionPane.showMessageDialog(null, "Não há nenhum amigo selecionado");
+            return;
+        }
+        if (JCBAluguel.getSelectedItem() == null) {
+            JOptionPane.showMessageDialog(null, "Não há nenhum aluguel selecionado");
+            return;
+        }
+    }//GEN-LAST:event_JBDevolverFerramentaActionPerformed
 
     /**
      * @param args the command line arguments
@@ -94,6 +182,13 @@ public class JFDevolverFerramenta extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton JBDevolverFerramenta;
+    private javax.swing.JComboBox<String> JCBAluguel;
+    private javax.swing.JComboBox<String> JCBAmigo;
+    private javax.swing.JLabel JLAluguel;
+    private javax.swing.JLabel JLAmigo;
+    private javax.swing.JLabel JLQuantidadeDevolver;
+    private javax.swing.JSpinner JSpinnerQuantidadeDevolver;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
